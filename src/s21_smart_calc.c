0301e99dd24696bec54ef54d412af398fe45f416
nikolay <russiakremlin242@gmail.com>
2024-01-05 05:01:41 +0300

    description of edge cases for plus and minus

--- src/s21_smart_calc.c
+++ src/s21_smart_calc.c
@@ -10,7 +10,7 @@
 
 #include "stack.h"
 #include "stdio.h"
-
+#define ARR_SIZE 100
 void printParsedData(parseData* data, int size);
 
 // функция которая сдвигает массив на данное число элементов в данную сторону
@@ -35,37 +35,49 @@ int main() {
   print("hi\n");
   // char s[9] = "sin(4)*4";
   // char s[30] = "2mod4*88.348+12";
-  char s[20] = "sin(--+30)";
+    int arrsize = 100;
+  char s[ARR_SIZE] = "sin(+4.24)*--sin(1)*tan(2.421)*-tan(1)* 1245";
 
   if(s[0] == '-') {
-    shiftArray(s, 0, 19, 1,1);
+    shiftArray(s, 0, arrsize-1, 1,1);
     s[0]='0';
   }
-    for(int i = 0; i < 19;++i) {
+    for(int i = 0; i < arrsize-1;++i) {
       if(s[i] == '-' && s[i+1] == '-') { // -- = +
-        shiftArray(s, i, 19, 1,0);
+        shiftArray(s, i, arrsize-1, 1,0);
         s[i] = '+';
         i--;
       }
     }
-    for(int i = 0; i < 19;++i) {
+    for(int i = 0; i < arrsize-1;++i) {
       if(s[i] == '+' && s[i+1] == '+') {
-        shiftArray(s, i, 19, 1,0);
+        shiftArray(s, i, arrsize-1, 1,0);
         i--;
       }
     }
 
     if(s[0] == '+') {
-      shiftArray(s, 0, 19, 1,1);
+      shiftArray(s, 0, arrsize-1, 1,1);
       s[0]='0';
     }
-    for(int i = 0; i < 19; i++) {
+    for(int i = 0; i < arrsize-1; i++) {
       if (s[i] == '(' && s[i+1] == '-') {
-        shiftArray(s, i, 19, 1,1);
+        shiftArray(s, i, arrsize-1, 1,1);
         s[i+1] = '0';
       }
     }
+/*
+  крайние случаи обработки некорректной строки
+   минус
+1    вначале строки - поставить между скобкой и минусом 0:  "-3*2(3)" - "0-3*2(3)"
+2    минус после скобки - поставить перед ним 0:  "3*2(-3)" - "3*2(0-3)"
+3    двойной минус - заменить на +: "3*2--3" - "3*2+3"
+  плюс
+4    двойной плюс - заменить на один плюс: "3++2+2" - "3+2"
+5    плюс между - * / и функцией - убрать: "3-+sin(2)" - "3-sin(2)"
+6    плюс после скобки - убрать: "3*(+2)" - "3*(2)"
 
+*/
   parseData* newexpression = {NULL};
   int size;
   newexpression = parser(s, &size);
