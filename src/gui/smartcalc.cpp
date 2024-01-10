3c3602046f31c58e12d613a45de60466bd8f3141
nikolay <russiakremlin242@gmail.com>
2024-01-11 01:21:15 +0300

    calc with graph

--- src/gui/smartcalc.cpp
+++ src/gui/smartcalc.cpp
@@ -14,14 +14,111 @@ using namespace std;
 SmartCalc::SmartCalc(QWidget* parent)
     : QMainWindow(parent), ui(new Ui::SmartCalc) {
   ui->setupUi(this);
-  h = 0.1;
-}
+  ui->grid->setInteraction(QCP::iRangeZoom, true);
+  ui->grid->setInteraction(QCP::iRangeDrag, true);
+  ui->grid->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+  // disable connecting dots with lines in grind plot
 
-SmartCalc::~SmartCalc() { delete ui; }
-void getVariablesFromPolish(stack* polish, variables* array[], int* arrayindex);
-void SmartCalc::on_pushButton_E_calc_clicked() {
-  calculate_Polish();
-  // здесь я собираюсь использовать функцию newprint
+
+ // setLineStyle(QCPGraph::lsNone);
+  // wGraphic = new QCustomPlot();
+  // ui->centralwidget->layout()->addWidget(wGraphic);
+   //connect(ui->grid, SIGNAL(QCustomPlot::plottableClick), this, SLOT(const QString& arg1));
+  // ui->gridLayout->addWidget(wGraphic,1,0,1,1);
+ // ui->grid->installEventFilter(this);
+
+  // plotting
+}
+// void SmartCalc::slotMouseMove(const QString& arg1) {
+//   /* Если при передвижении мыши, ей кнопки нажаты,
+//    * то вызываем отработку координат мыши
+//    * через слот клика
+//    * */
+//   cout << "moved";
+//   string test = "moved";
+//   ui->lineEdit_answer->setText(test.c_str());
+//   if (QApplication::mouseButtons()) {
+//     // plot();
+//   }
+// }
+void SmartCalc::plot() {
+  x.clear();
+  y.clear();
+  ui->grid->addGraph();
+  QCPGraph *graph = ui->grid->graph(0); // Предполагая, что вы имеете один график
+  // Отключаем отображение линий между точками на графике
+  ui->grid->graph(0)->clearData();
+  ui->grid->replot();
+
+  //use dots instead of lines
+  // graph->setLineStyle(QCPGraph::lsNone);
+  // QCPScatterStyle scatterStyle;
+  // scatterStyle.setShape(QCPScatterStyle::ssCircle);
+  // scatterStyle.setPen(QPen(Qt::blue, 2));
+  // scatterStyle.setBrush(Qt::blue);
+  // scatterStyle.setSize(1);
+  // graph->setScatterStyle(scatterStyle); //
+  //
+  // QCPItemLine *line1 = new QCPItemLine(ui->grid);
+// //create a 10 lines in cyle and then add it to grid
+//   for (int i = 0; i < 10; i++) {
+//       QCPItemLine *line = new QCPItemLine(ui->grid);
+//       // Set the coordinates for the line
+//       line->start->setCoords(x1, y1); // Set the starting point coordinates (x1, y1)
+//       line->end->setCoords(x2, y2);   // Set the ending point coordinates (x2, y2)
+//
+//       // Customize the line properties (color, style, etc.)
+//       line->setPen(QPen(Qt::black));  // Set the line color to black
+//
+//       // Add the line to the grid
+//       ui->grid->addItem(line);
+//   }
+
+
+  //
+  // // Устанавливаем координаты начала и конца для первого отрезка
+  // line1->start->setCoords(1, 2); // Начальные координаты отрезка (x, y)
+  // line1->end->setCoords(4, 5);   // Конечные координаты отрезка (x, y)
+  //
+  // // Устанавливаем цвет и стиль для первого отрезка
+  // line1->setPen(QPen(Qt::red));  // Цвет линии
+  //
+  // // Добавляем отрезки на график
+  // ui->grid->addItem(line1);
+
+ // ui->grid->xAxis->setRange(-4, 4);
+  //ui->grid->yAxis->setRange(0, 9);
+  QCPAxisRect *axisRect = ui->grid->axisRect();
+  QCPRange xrange = axisRect->axis(QCPAxis::atBottom)->range();
+//  QCPRange yrange = axisRect->axis(QCPAxis::atLeft)->range();
+  xBegin = ui->grid->xAxis->range().lower -(ui->grid->xAxis->range().upper -ui->grid->xAxis->range().lower)*3;
+  xEnd = ui->grid->xAxis->range().upper  + (ui->grid->xAxis->range().upper -ui->grid->xAxis->range().lower)*3;
+  h = (xEnd- xBegin)/50000;;
+   std::string expression = ui->lineEdit_expression->text().toStdString();
+  string readystr ;
+  Point<int, int> p{};
+  QVector<Point<long double, long double>> points;
+  N = (xEnd - xBegin) / h + 2;
+
+  // readystr = replaceVars(expression, X);
+  // calc_s prev = parseAndCalc(readystr);
+  //
+  for (X = xBegin; X <= xEnd; X += h) {
+
+    x.push_back(X);
+    readystr = replaceVars(expression, X);
+    calc_s ans = parseAndCalc(readystr);
+    if(ans.e ==E_NO_ERRORS) {
+      y.push_back(ans.n);
+
+    }
+
+  }
+  // ui->grid->graph()->clearData();
+
+  ui->grid->addGraph();
+  ui->grid->graph(0)->addData(x, y);
+  ui->grid->replot();
 }
 std::string longDoubleToString(long double n) {
   char buffer[100];
@@ -34,6 +131,42 @@ std::string longDoubleToString(long double n) {
     return "Error: Unable to convert long double to string";
   }
 }
+string SmartCalc::replaceVars(string expression, long double x) {
+  string tmplt = "x";
+  for (int i = 0; i < expression.size(); i++)
+    if (expression[i] == 'x')
+      expression.replace(i, 1, longDoubleToString(x));
+  return expression;
+}
+calc_s SmartCalc::parseAndCalc(const std::string& expression) {
+  int size, error, xExists = 0;
+
+  parseData* parsedExpression = {nullptr};
+  parsedExpression = parser((char*)expression.c_str(), &size);
+  error = check(parsedExpression, size, (char*)expression.c_str());
+  if (error != E_NO_ERRORS && error != E_ONLY_DIGITS_EXIST||
+      parsedExpression[0].type == E_INCORRECT_EXPRESSION ) {
+    calc_s ans = {0, error != E_NO_ERRORS ? error : E_INCORRECT_EXPRESSION};
+    return ans;
+  }
+  stack notation, reversed;
+  stackInit(&notation), stackInit(&reversed);
+  notation = evaluatePolishNotation((char*)expression.c_str());
+  while (notation.stSize) push(&reversed, pop(&notation));
+  string notationStr = polishToString(&reversed);
+  calc_s ans = calcPolishNotation(&reversed);
+
+  free(parsedExpression);
+  freeStack(&notation);
+  freeStack(&reversed);
+  return ans;
+}
+SmartCalc::~SmartCalc() { delete ui; }
+void getVariablesFromPolish(stack* polish, variables* array[], int* arrayindex);
+void SmartCalc::on_pushButton_E_calc_clicked() {
+  calculate_Polish();
+  // здесь я собираюсь использовать функцию newprint
+}
 
 void SmartCalc::calculate_Polish() {
   std::string infotext;
@@ -43,7 +176,8 @@ void SmartCalc::calculate_Polish() {
   int size, error, xExists = 0;
   string tmplt = "x";
   for (int i = 0; i < expression.size(); i++) {
-    if (expression[i] == 'x') {xExists=1;
+    if (expression[i] == 'x') {
+      xExists = 1;
       expression.replace(i, 1, ui->lineEdit_X_value->text().toStdString());
     }
   }
@@ -51,10 +185,10 @@ void SmartCalc::calculate_Polish() {
   parsedExpression = parser((char*)expression.c_str(), &size);  // TODO CLEAR
   error = check(parsedExpression, size, (char*)expression.c_str());
   if (error != E_NO_ERRORS ||
-      parsedExpression[0].type == E_INCORRECT_EXPRESSION || xExists == 1 && Xvalues.empty()) {
+      parsedExpression[0].type == E_INCORRECT_EXPRESSION ||
+      xExists == 1 && Xvalues.empty()) {
     // printf("Error - %s", errorDescription(error));
-    calc_s ans = {0,
-                  error != E_NO_ERRORS ? error : E_INCORRECT_EXPRESSION};
+    calc_s ans = {0, error != E_NO_ERRORS ? error : E_INCORRECT_EXPRESSION};
     infotext = errorDescription(ans.e);
     answer = NAN;
     // return ans.n;
@@ -79,7 +213,6 @@ void SmartCalc::calculate_Polish() {
     free(parsedExpression);
     freeStack(&notation);
     freeStack(&reversed);
-
   }
   ui->lineEdit_answer->setText(answer.c_str());
   ui->lineEdit_infotext->setText(infotext.c_str());
@@ -171,210 +304,82 @@ void SmartCalc::on_lineEdit_expression_textChanged(const QString& arg1) {
   if (ui->checkBox_lifetime_calc->checkState() == Qt::Checked)
     calculate_Polish();
 }
-void SmartCalc::on_lineEdit_X_value_textChanged(const QString &arg1)
-{
-    if (ui->checkBox_lifetime_calc->checkState() == Qt::Checked)
-      calculate_Polish();
+void SmartCalc::on_lineEdit_X_value_textChanged(const QString& arg1) {
+  if (ui->checkBox_lifetime_calc->checkState() == Qt::Checked)
+    calculate_Polish();
 }
 
-void SmartCalc::on_pushButton_D_1_clicked()
-{
-    changeStringAdd("1");
-}
+void SmartCalc::on_pushButton_D_1_clicked() { changeStringAdd("1"); }
 
 void SmartCalc::changeStringAdd(const string& str) {
-  string curr  = ui->lineEdit_expression->text().toStdString();
-  ui->lineEdit_expression->setText((curr+str).c_str());
+  string curr = ui->lineEdit_expression->text().toStdString();
+  ui->lineEdit_expression->setText((curr + str).c_str());
 }
 
+void SmartCalc::on_pushButton_D_0_clicked() { changeStringAdd("0"); }
 
+void SmartCalc::on_pushButton_D_dot_clicked() { changeStringAdd("."); }
 
+void SmartCalc::on_pushButton_M_div_clicked() { changeStringAdd("/"); }
 
+void SmartCalc::on_pushButton_D_2_clicked() { changeStringAdd("2"); }
 
+void SmartCalc::on_pushButton_D_3_clicked() { changeStringAdd("3"); }
 
-void SmartCalc::on_pushButton_D_0_clicked()
-{
-        changeStringAdd("0");
-}
+void SmartCalc::on_pushButton_M_mul_clicked() { changeStringAdd("*"); }
 
+void SmartCalc::on_pushButton_D_4_clicked() { changeStringAdd("4"); }
 
-void SmartCalc::on_pushButton_D_dot_clicked()
-{
-    changeStringAdd(".");
-}
+void SmartCalc::on_pushButton_D_5_clicked() { changeStringAdd("5"); }
 
+void SmartCalc::on_pushButton_D_6_clicked() { changeStringAdd("6"); }
 
-void SmartCalc::on_pushButton_M_div_clicked()
-{
-        changeStringAdd("/");
-}
+void SmartCalc::on_pushButton_M_sub_clicked() { changeStringAdd("-"); }
 
+void SmartCalc::on_pushButton_D_7_clicked() { changeStringAdd("7"); }
 
-void SmartCalc::on_pushButton_D_2_clicked()
-{
-        changeStringAdd("2");
-}
+void SmartCalc::on_pushButton_D_8_clicked() { changeStringAdd("8"); }
 
+void SmartCalc::on_pushButton_D_9_clicked() { changeStringAdd("9"); }
 
-void SmartCalc::on_pushButton_D_3_clicked()
-{
-        changeStringAdd("3");
-}
+void SmartCalc::on_pushButton_M_add_clicked() { changeStringAdd("+"); }
 
+void SmartCalc::on_pushButton_bracket_o_clicked() { changeStringAdd("("); }
 
-void SmartCalc::on_pushButton_M_mul_clicked()
-{
-        changeStringAdd("*");
-}
+void SmartCalc::on_pushButton_bracket_c_clicked() { changeStringAdd(")"); }
 
+void SmartCalc::on_pushButton_constant_e_clicked() { changeStringAdd("e"); }
 
-void SmartCalc::on_pushButton_D_4_clicked()
-{
-        changeStringAdd("4");
-}
+void SmartCalc::on_pushButton_constant_pi_clicked() { changeStringAdd("pi"); }
 
+void SmartCalc::on_pushButton_M_mod_clicked() { changeStringAdd("mod"); }
 
-void SmartCalc::on_pushButton_D_5_clicked()
-{
-            changeStringAdd("5");
-}
+void SmartCalc::on_pushButton_M_2sqrt_clicked() { changeStringAdd("sqrt"); }
 
+void SmartCalc::on_pushButton_M_pow_clicked() { changeStringAdd("^"); }
 
-void SmartCalc::on_pushButton_D_6_clicked()
-{
-            changeStringAdd("6");
-}
-
-
-void SmartCalc::on_pushButton_M_sub_clicked()
-{
-            changeStringAdd("-");
-}
-
-
-void SmartCalc::on_pushButton_D_7_clicked()
-{
-            changeStringAdd("7");
-}
-
-
-void SmartCalc::on_pushButton_D_8_clicked()
-{
-            changeStringAdd("8");
-}
-
-
-void SmartCalc::on_pushButton_D_9_clicked()
-{
-            changeStringAdd("9");
-}
-
-
-void SmartCalc::on_pushButton_M_add_clicked()
-{
-            changeStringAdd("+");
-}
-
-
-void SmartCalc::on_pushButton_bracket_o_clicked()
-{
-        changeStringAdd("(");
-}
-
-
-void SmartCalc::on_pushButton_bracket_c_clicked()
-{
-            changeStringAdd(")");
-}
-
-
-void SmartCalc::on_pushButton_constant_e_clicked()
-{
-            changeStringAdd("e");
-}
-
-
-void SmartCalc::on_pushButton_constant_pi_clicked()
-{
-            changeStringAdd("pi");
-}
-
-
-void SmartCalc::on_pushButton_M_mod_clicked()
-{
-            changeStringAdd("mod");
-}
-
-
-void SmartCalc::on_pushButton_M_2sqrt_clicked()
-{
-            changeStringAdd("sqrt");
-}
-
-
-void SmartCalc::on_pushButton_M_pow_clicked()
-{
-            changeStringAdd("^");
-}
-
-
-void SmartCalc::on_pushButton_E_clean_clicked()
-{
+void SmartCalc::on_pushButton_E_clean_clicked() {
   ui->lineEdit_expression->setText("");
-
 }
 
+void SmartCalc::on_pushButton_M_asin_clicked() { changeStringAdd("asin"); }
 
-void SmartCalc::on_pushButton_M_asin_clicked()
-{
-            changeStringAdd("asin");
+void SmartCalc::on_pushButton_M_acos_clicked() { changeStringAdd("acos"); }
+
+void SmartCalc::on_pushButton_M_atan_clicked() { changeStringAdd("atan"); }
+
+void SmartCalc::on_pushButton_M_log_clicked() { changeStringAdd("log"); }
+
+void SmartCalc::on_pushButton_M_sin_clicked() { changeStringAdd("sin"); }
+
+void SmartCalc::on_pushButton_M_cos_clicked() { changeStringAdd("cos"); }
+
+void SmartCalc::on_pushButton_M_tan_clicked() { changeStringAdd("tan"); }
+
+void SmartCalc::on_pushButton_M_ln_clicked() { changeStringAdd("ln"); }
+
+void SmartCalc::on_pushButton_V_graph_clicked() {// changeStringAdd("plot");
+  plot();
 }
 
-
-void SmartCalc::on_pushButton_M_acos_clicked()
-{
-            changeStringAdd("acos");
-}
-
-
-void SmartCalc::on_pushButton_M_atan_clicked()
-{
-            changeStringAdd("atan");
-}
-
-
-void SmartCalc::on_pushButton_M_log_clicked()
-{
-            changeStringAdd("log");
-}
-
-
-void SmartCalc::on_pushButton_M_sin_clicked()
-{
-            changeStringAdd("sin");
-}
-
-
-void SmartCalc::on_pushButton_M_cos_clicked()
-{
-            changeStringAdd("cos");
-}
-
-
-void SmartCalc::on_pushButton_M_tan_clicked()
-{
-            changeStringAdd("tan");
-}
-
-
-void SmartCalc::on_pushButton_M_ln_clicked()
-{
-            changeStringAdd("ln");
-}
-
-
-void SmartCalc::on_pushButton_V_graph_clicked()
-{
-            changeStringAdd("plot");
-}
 
--- src/gui/smartcalc.h
+++ src/gui/smartcalc.h
@@ -5,12 +5,18 @@
 #include <QVector>
 #include <string>
 #include <iostream>
+#include "qcustomplot.h"
 #define CHEADER_H_INCLUDED
 extern "C" {
 #include "../backend/stack.h"
 }
 #endif
 
+template <typename T, typename T2>
+struct Point {
+  T x;
+  T2 y;
+};
 typedef struct vars {
   std::string name;
   long double value;
@@ -34,6 +40,7 @@ public:
     ~SmartCalc();
 
 private slots:
+
     void on_pushButton_E_calc_clicked();
     void calculate_Polish();
     // void on_lineEdit_expression_textChanged(const QString &arg1);
@@ -44,76 +51,52 @@ private slots:
   static std::vector<std::string> Variables(parseData* data, int size) ;
   static std::string polishToString(stack* parsedExpression) ;
   void changeStringAdd(const std::string& str);
+  static calc_s parseAndCalc(const std::string& expression);
+  void plot();
+ std::string replaceVars(std::string expression, long double x);
+
   void on_lineEdit_expression_textChanged(const QString &arg1);
   void on_lineEdit_X_value_textChanged(const QString &arg1);
   void on_pushButton_D_1_clicked();
-
   void on_pushButton_D_0_clicked();
-
   void on_pushButton_D_dot_clicked();
-
   void on_pushButton_M_div_clicked();
-
   void on_pushButton_D_2_clicked();
-
   void on_pushButton_D_3_clicked();
-
   void on_pushButton_M_mul_clicked();
-
   void on_pushButton_D_4_clicked();
-
   void on_pushButton_D_5_clicked();
-
   void on_pushButton_D_6_clicked();
-
   void on_pushButton_M_sub_clicked();
-
   void on_pushButton_D_7_clicked();
-
   void on_pushButton_D_8_clicked();
-
   void on_pushButton_D_9_clicked();
-
   void on_pushButton_M_add_clicked();
-
   void on_pushButton_bracket_o_clicked();
-
   void on_pushButton_bracket_c_clicked();
-
   void on_pushButton_constant_e_clicked();
-
   void on_pushButton_constant_pi_clicked();
-
   void on_pushButton_M_mod_clicked();
-
   void on_pushButton_M_2sqrt_clicked();
-
   void on_pushButton_M_pow_clicked();
-
   void on_pushButton_E_clean_clicked();
-
   void on_pushButton_M_asin_clicked();
-
   void on_pushButton_M_acos_clicked();
-
   void on_pushButton_M_atan_clicked();
-
   void on_pushButton_M_log_clicked();
-
   void on_pushButton_M_sin_clicked();
-
   void on_pushButton_M_cos_clicked();
-
   void on_pushButton_M_tan_clicked();
-
   void on_pushButton_M_ln_clicked();
-
   void on_pushButton_V_graph_clicked();
 
 private:
     Ui::SmartCalc *ui;
-    double xBegin, xEnd, h,X;
+  QCustomPlot *wGraphic;      // Объявляем объект QCustomPlot
+  QCPCurve *verticalLine;
+    double xBegin, xEnd, h, X;
     int N;
     QVector<double> x,y;
+  void slotMouseMove(const QString& arg1);
 };
 #endif // SMARTCALC_H
